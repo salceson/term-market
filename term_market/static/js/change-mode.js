1a1baa5835a81ded494d5308150ee0e45bbbd75a
Joanna Gajda <joannagajda5@gmail.com>
2015-05-17 15:25:11 +0200

    Persist skin preference in cookie

--- term_market/static/js/change-mode.js
+++ term_market/static/js/change-mode.js
@@ -2,17 +2,25 @@
  * Created by Joanna on 2015-05-12.
  */
 
-var nightMode = true;
+var link = document.getElementById("css-skin");
+var nightCSS = Django.static('css/cover.css');
+var dayCSS = Django.static('css/cover-light.css');
 
 function changeCSS() {
     nightMode = !nightMode;
-    var nightCSS = Django.static('css/cover.css');
-    var dayCSS = Django.static('css/cover-light.css');
-    var link = document.getElementById("css-skin")
 
     if (nightMode) {
+        $.cookie('skin', 'night', { path: '/', expires: 3650 });
         link.href = nightCSS;
     } else {
+        $.cookie('skin', 'day', { path: '/', expires: 3650 });
         link.href = dayCSS;
     }
+}
+
+var nightMode = ($.cookie('skin') != 'day');
+if (nightMode) {
+    link.href = nightCSS;
+} else {
+    link.href = dayCSS;
 }
