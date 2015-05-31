408e24469b334a0dd4f29d8474020203b48f69aa
Mateusz Malek <mamalek@student.agh.edu.pl>
2015-05-31 18:50:31 +0200

    [GIELDA-49] Prefetch related in available offers views

--- term_market/views.py
+++ term_market/views.py
@@ -113,6 +113,7 @@ class AvailableOffersMixin(object):
     def get_queryset(self):
         qs = super(AvailableOffersMixin, self).get_queryset()
         qs = qs.filter(term__subject=F('offer__offered_term__subject'), term__in=self.request.user.terms.all())
+        qs = qs.select_related()
         qs = qs.exclude(offer__donor=self.request.user)
         qs = qs.exclude(offer__offered_term__in=self.request.user.terms.values_list('conflicting_terms', flat=True))
         qs = qs.order_by('offer')
