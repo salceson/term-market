0635d98386b5a245073bd4e0570eda2fd82b8490
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2015-06-07 23:58:11 +0200

    [GIELDA-54] First steps in running the solver GUI.

--- term_market/solver.py
+++ term_market/solver.py
@@ -2,10 +2,14 @@
 from uuid import uuid4 as random_uuid
 
 from django.contrib.auth.decorators import permission_required
+from django.core.urlresolvers import reverse
+from django.forms import Form
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from django.views.generic import FormView, TemplateView
 
 from .models import Enrollment, Offer, Term
+from .views import PermissionRequiredMixin
 
 
 @permission_required("term_market.change_enrollment")
@@ -39,3 +43,35 @@ def get_offers(enrollment):
             "wanted_terms": map(lambda o: o.id, offer.wanted_terms.all())
         })
     return {"offers": offers_list}
+
+
+class ManualSolverRunView(PermissionRequiredMixin, FormView):
+    template_name = "term_market/admin/solver_run.html"
+    object = Enrollment
+    permission_required = 'term_market.change_enrollment'
+    form_class = Form
+
+    def get_context_data(self, **kwargs):
+        context = super(ManualSolverRunView, self).get_context_data(**kwargs)
+        enrollment = get_object_or_404(Enrollment, id=self.kwargs['enrollment'])
+        context.update({'title': 'Run solver', 'enrollment_name': enrollment.name})
+        return context
+
+    def form_valid(self, form):
+        # TODO: Proper actions here (running the solver)
+        return super(ManualSolverRunView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('solver_running', kwargs={'enrollment': self.kwargs['enrollment']})
+
+
+class ManualSolverRunningView(PermissionRequiredMixin, TemplateView):
+    template_name = "term_market/admin/solver_running.html"
+    object = Enrollment
+    permission_required = 'term_market.change_enrollment'
+
+    def get_context_data(self, **kwargs):
+        context = super(ManualSolverRunningView, self).get_context_data(**kwargs)
+        enrollment = get_object_or_404(Enrollment, id=self.kwargs['enrollment'])
+        context.update({'title': 'Run solver', 'enrollment_name': enrollment.name})
+        return context
