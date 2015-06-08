3cc1393a71aca92ede27784391ec04e86f26afe1
Mateusz Malek <mamalek@student.agh.edu.pl>
2015-06-08 10:25:36 +0200

    Moarge migrations conflict.

---
+++ term_market/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('term_market', '0007_auto_20150602_1908'),
+        ('term_market', '0008_auto_20150608_0741'),
+    ]
+
+    operations = [
+    ]
